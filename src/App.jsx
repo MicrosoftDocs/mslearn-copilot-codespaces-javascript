a63a4f99db3348cd252d17e10b6bada2b6183a46
Oliver Fabián Stetcu Stepanov <100787553+ZiwiiDev@users.noreply.github.com>
2023-11-07 17:17:13 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Oliver Fabian Stetcu Stepanov",
+  title: "Full Stack Developer",
+  email: "oliverfabian22@gmail.com",
+  gitHub: "ZiwiiDev",
+  instagram: "@oliveristhenamx",
+  linkedIn: "Oliver Fabian Stetcu",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "NotZiuenn",
+  youTube: "Creative Tones",
 };
 
 const primaryColor = "#4E567E";
