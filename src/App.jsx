4fe502985a8bc54c7b551714edba3c5b019087d8
bbaez <bbaez.dev@gmail.com>
2024-06-12 03:27:00 +0000

    chore: Update contact information and add social icon animation

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Benjamin Baez",
+  title: "Software Developer and web designer",
+  email: "bbaez.dev@gmail.com",
+  gitHub: "bbaezdev",
+  instagram: "benjvx._/",
+  linkedIn: "benjam%C3%ADn-baez-439592313/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
