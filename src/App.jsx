88479f46c637f0830baedb44296d91d4f5b36c06
Brayan Torres <brato84@hotmail.com>
2024-06-13 22:01:02 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Brayan Torres",
+  title: "Cybersecurity & AWS",
+  email: "brato84@gmail.com",
+  gitHub: "brato84",
+  instagram: "brato84",
+  linkedIn: "brato84",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "brato84",
+  youTube: "brato84",
 };
 
 const primaryColor = "#4E567E";
